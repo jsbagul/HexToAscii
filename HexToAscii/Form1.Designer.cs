52d612f0232137f581de7ac95e1b64e3e6af4fe0
Jayesh Bagul <jbagul@chemito.net>
2024-10-15 11:52:38 +0530

    Inital Commit

---
+++ HexToAscii/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace HexToAscii
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBInput = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblDisplay = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtBInput
+            // 
+            this.txtBInput.Location = new System.Drawing.Point(233, 89);
+            this.txtBInput.Multiline = true;
+            this.txtBInput.Name = "txtBInput";
+            this.txtBInput.Size = new System.Drawing.Size(160, 32);
+            this.txtBInput.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(467, 98);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 1;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblDisplay
+            // 
+            this.lblDisplay.AutoSize = true;
+            this.lblDisplay.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDisplay.Location = new System.Drawing.Point(326, 204);
+            this.lblDisplay.Name = "lblDisplay";
+            this.lblDisplay.Size = new System.Drawing.Size(20, 25);
+            this.lblDisplay.TabIndex = 2;
+            this.lblDisplay.Text = "-";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblDisplay);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtBInput);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBInput;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblDisplay;
+    }
+}
+
---
+++ HexToAscii/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HexToAscii
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            string inputTxt = txtBInput.Text;
+            string asciiOutput = HexToAscii(inputTxt);
+            lblDisplay.Text = "ASCII: " + asciiOutput;
+        }
+        private string HexToAscii(string hex)
+        {
+            // Remove spaces if any
+            hex = hex.Replace(" ", "");
+
+            // Make sure the input length is even
+            if (hex.Length % 2 != 0)
+                throw new ArgumentException("Invalid Hex string");
+
+            StringBuilder ascii = new StringBuilder();
+
+            for (int i = 0; i < hex.Length; i += 2)
+            {
+                // Convert each hex pair to a byte
+                string hexPair = hex.Substring(i, 2);
+                byte byteValue = Convert.ToByte(hexPair, 16);
+
+                // Convert byte to ASCII character and append to the string
+                ascii.Append((char)byteValue);
+            }
+
+            return ascii.ToString();
+        }
+    }
+}
